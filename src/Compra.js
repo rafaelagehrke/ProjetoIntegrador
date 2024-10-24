a7c99e85a9282c1e4c99588070701906929e6c71
Felipe Motta <felipesoldatellimotta@gmail.com>
2024-10-24 16:33:50 -0300

    Removido node_modules e adicionado o .gitignore.

--- src/Compra.js
+++ src/Compra.js
@@ -1,6 +1,22 @@
 import './App.css';
+import express from "express";
 
 function Compra() {
+
+  const express = require('express');
+
+  const server = express(); 
+
+  server.use(express.json());
+  server.use(express.urlencoded({extended: true}));
+
+  server.listen(3001, ()=> {console.log('servidor rolando')});
+
+  server.get('/calcula-ticket', (req, res) => {
+    const total = req.body.total;
+    res.send(`${total} é o total.`);
+  });
+
   return (
     <div className="align-items-left">
         <p>Compra</p> 
